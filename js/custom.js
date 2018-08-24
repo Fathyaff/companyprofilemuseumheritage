eb55a8431afe20608ca04c381b0498af43425919
fathyaff <fathya.afifah@kokatto.com>
2018-08-24 20:12:53 +0700

    update background etc

--- js/custom.js
+++ js/custom.js
@@ -57,9 +57,7 @@
       // else:
 
       images: [
-        "img/bgslides/1.jpg",
-        "img/bgslides/2.jpg",
-        "img/bgslides/3.jpg"
+        "img/asets/bg-1.png",
       ],
 
       onStart: function() {},
